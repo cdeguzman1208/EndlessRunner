5e422e723ac505519332f394a86cc80b7a0949b7
Cromwell <cdeguzman1208@gmail.com>
2023-05-02 22:21:30 -0700

    main config

--- src/main.js
+++ src/main.js
@@ -6,20 +6,31 @@ Tilt:
 
 */
 
+// tame the javashrek
+'use strict';
+
+// global variables
+let cursors;
+let currentScene = 0;
+const SCALE = 0.5;
+const tileSize = 35;
+
+// main game object
 let config  = {
     type: Phaser.CANVAS,
     width: 640,
     height: 480,
-    scene: [ Menu, Play ]
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: true,
+            gravity: {
+                x: 0,
+                y: 0
+            }
+        }
+    },
+    scene: [ Loading, Menu, Levels, Play, Pause, Credits ]
 }
 
 let game = new Phaser.Game(config);
-
-// reserve keyboard vars
-let keyW, keyA, keyS, keyD, keyUP, keyLEFT, keyDOWN, keyRIGHT, keySPACE;
-
-// set UI
-let borderUISize = game.config.height / 15;
-let borderPadding = borderUISize / 3;
-
-let highscore = 0;
---
+++ src/scenes/Loading.js
@@ -0,0 +1,17 @@
+class Loading extends Phaser.Scene {
+    constructor() {
+        super('loadingScene');
+    }
+
+    preload() {
+        
+    }
+
+    create() {
+        
+    }
+
+    update() {
+
+    }
+}
