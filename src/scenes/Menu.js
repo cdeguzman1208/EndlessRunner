cc7ec074673439701a5659f116cbe8b1fd9b5a44
Cromwell <cdeguzman1208@gmail.com>
2023-05-09 15:05:18 -0700

    update menu

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -17,6 +17,9 @@ class Menu extends Phaser.Scene {
             fixedWidth: 0
         }
         this.add.text(game.config.width/2, game.config.height/2, 'ENDLESS RUNNER', menuConfig).setOrigin(0.5);
+        menuConfig.fontSize = '20px';
+        menuConfig.color = 'white';
+        this.add.text(game.config.width/2, game.config.height/2 + 75, 'press space to play, backpace for credits, esc for main menu', menuConfig).setOrigin(0.5);
         
         // set up scene switcher
         this.input.keyboard.on('keydown', (event) => {
